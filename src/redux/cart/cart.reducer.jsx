edcf445043f86706d9c8dbc7a21320b15e80c990
Yusuf Ganiyu <airscholar@gmail.com>
2022-06-27 17:00:12 +0100

    chore: added cart actions and reducers

--- src/redux/cart/cart.reducer.jsx
+++ src/redux/cart/cart.reducer.jsx
@@ -1,5 +1,5 @@
 import { CartActionTypes } from './cart.types';
-import { addItemToCart } from './cart.utils';
+import { addItemToCart, IncDecCartItemQuantity, removeItemFromCart } from './cart.utils';
 
 const INITIAL_STATE = {
     cartItems: [],
@@ -18,6 +18,24 @@ export const cartReducer = (state = INITIAL_STATE, action) => {
                 ...state,
                 cartItems: addItemToCart(state.cartItems, action.payload),
             }
+        case CartActionTypes.INCREASE_CART_ITEM_QUANTITY: {
+            return {
+                ...state,
+                cartItems: IncDecCartItemQuantity(state.cartItems, action.payload, 'inc')
+            }
+        }
+        case CartActionTypes.DECREASE_CART_ITEM_QUANTITY: {
+            return {
+                ...state,
+                cartItems: IncDecCartItemQuantity(state.cartItems, action.payload, 'dec')
+            }
+        }
+        case CartActionTypes.REMOVE_ITEM_FROM_CART: {
+            return {
+                ...state,
+                cartItems: removeItemFromCart(state.cartItems, action.payload)
+            }
+        }
         default:
             return state;
     }
