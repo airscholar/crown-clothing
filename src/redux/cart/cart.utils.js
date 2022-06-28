cbf1e98e6730825a76e5fde36e141ab78b9a86c7
Yusuf Ganiyu <airscholar@gmail.com>
2022-06-28 21:16:44 +0100

    chore: code refactor

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,7 +1,5 @@
 export const addItemToCart = (cartItems, itemToAdd) => {
-  //   cartItems = Array.from(cartItems);
   const existingItem = cartItems.find(cartItem => cartItem.id === itemToAdd.id);
-  //   console.log({ existingItem, cartItems });
   if (existingItem) {
     return cartItems.map(item =>
       item.id === itemToAdd.id ? { ...item, quantity: item.quantity + 1 } : item
