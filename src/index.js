6d7553ca8c2787a9dc4efb34c06336df8fb5ebe9
Yusuf Ganiyu <airscholar@gmail.com>
2022-06-29 21:40:12 +0100

    feat: added session storage to persist redux store

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from './redux/store';
+import { store, persistor } from './redux/store';
+import { PersistGate } from 'redux-persist/lib/integration/react';
 
 import './index.css';
 import App from './App';
@@ -12,6 +13,7 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
+        <PersistGate persistor={persistor}></PersistGate>
         <App />
       </BrowserRouter>
     </Provider>
