7c8835f2ae62c5fcdf92f80d68bbbaa2b098e23a
Yusuf Ganiyu <airscholar@gmail.com>
2022-07-04 10:44:27 +0100

    Create constant.js

---
+++ src/firebase/constant.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyD4g7dJsasdasdzu_eB8IaMTgFuasdasd',
+  authDomain: 'crown-aaSA.firebaseapp.com',
+  projectId: 'crown-clothi',
+  storageBucket: 'crown-cloth.com',
+  messagingSenderId: '251111511112',
+  appId: '1:256742981658:web:863asdsadasd',
+  measurementId: 'G-SWYMNsdf23H',
+};
+
+export default firebaseConfig;
