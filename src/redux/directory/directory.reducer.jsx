2e4b6ff32525e98838125423ee7fe31763dbbe57
Yusuf Ganiyu <airscholar@gmail.com>
2022-07-26 11:37:35 +0100

    chore: restyled header component to use styled components

--- src/redux/directory/directory.reducer.jsx
+++ src/redux/directory/directory.reducer.jsx
@@ -3,7 +3,7 @@ const INITIAL_STATE = {
         { id: 1, title: "HATS", subtitle: "SHOW NOW", imageUrl: "https://i.ibb.co/cvpntL1/hats.png", linkUrl: 'shop/hats' },
         { id: 2, title: "JACKETS", subtitle: "SHOW NOW", imageUrl: "https://i.ibb.co/px2tCc3/jackets.png", linkUrl: 'shop/jackets' },
         { id: 3, title: "SNEAKERS", subtitle: "SHOW NOW", imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png", linkUrl: 'shop/sneakers' },
-        { id: 4, title: "WOMENS", subtitle: "SHOW NOW", imageUrl: "https://i.ibb.co/GCCdy8t/womens.png", size: 'large', linkUrl: 'shop/women' },
+        { id: 4, title: "WOMENS", subtitle: "SHOW NOW", imageUrl: "https://i.ibb.co/GCCdy8t/womens.png", size: 'large', linkUrl: 'shop/womens' },
         { id: 5, title: "MENS", subtitle: "SHOW NOW", imageUrl: "https://i.ibb.co/R70vBrQ/men.png", size: 'large', linkUrl: 'shop/mens' },
     ]
 }
