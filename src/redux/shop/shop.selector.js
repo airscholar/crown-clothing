ce04426c178c42295ff89408f41c3fe2de0cd9c6
Yusuf Ganiyu <airscholar@gmail.com>
2022-08-22 11:24:47 +0100

    feat: update redux to fetch from firebase

--- src/redux/shop/shop.selector.js
+++ src/redux/shop/shop.selector.js
@@ -3,3 +3,7 @@ import { createSelector } from 'reselect';
 const selectShop = state => state.shop;
 
 export const selectShopCollections = createSelector([selectShop], shop => shop.collections);
+export const selectShopCollectionsForPreview = createSelector(
+  [selectShopCollections],
+  collections => Object.keys(collections).map(key => collections[key])
+);
